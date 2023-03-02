5ecefd8443b2a80762751b4e8b1a4741ac67d811
Natalia <averegina@male.ru>
2023-03-02 18:12:58 +0300

    conf

--- master.py
+++ master.py
@@ -3,9 +3,7 @@ import user_interface as interface
 
 print(f'\n{messages.MSG1.center(len(messages.MSG8))}\n{messages.MSG8}')
 
-
 def invitation():
     interface.programm_action_choice()
-
 
 invitation()
